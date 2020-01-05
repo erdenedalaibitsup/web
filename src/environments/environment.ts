749ccd9afe9399be7248a413971c1226ce7640a2
Erdenedalaibitsup <Ae944990882>
2020-01-05 20:16:31 +0800

    prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase : {
     apiKey: "AIzaSyBrgyNdi9I57jT7ZIvsoih2nMASXiJknj0",
     authDomain: "taxicaling.firebaseapp.com",
